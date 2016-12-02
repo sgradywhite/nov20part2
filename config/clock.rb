5523b989cab8f39837ab242b72c549e926dcae08
Stephan White <sgradywhite@yahoo.com>
2016-12-02 03:58:20 +0000

    fix the find in clock.rb

--- config/clock.rb
+++ config/clock.rb
@@ -13,12 +13,12 @@ every(30.seconds, 'Creating Cycle') {
 
       Appointment.all.each do |appointment|
 
-        for i in  0..100 do
+        
 
-              AppointmentMailer.confirm_appointment(User.where(:email => "sgradywhite@yahoo.com"), appointment.find(i).created_at, appointment.find(i).status).deliver_now
+              AppointmentMailer.confirm_appointment(User.where(:email => "sgradywhite@yahoo.com"), appointment.find(1).created_at, appointment.find(1).status).deliver_now
 
 
-        end
+        
 
       end
 
